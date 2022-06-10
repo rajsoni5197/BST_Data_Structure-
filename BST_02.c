5fbd64e3c434f411cdaa792f2f3ad43416774156
Raj soni <74926805+rajsoni5197@users.noreply.github.com>
2022-06-10 17:43:28 +0530

    same file as bst_01.c but with added functions.
    added function : 
    1. findMax 
    2. findMin 
    3. search
    4. findheight.

---
+++ BST_02.c
@@ -0,0 +1,169 @@
+//implementing a binary search tree in c.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+struct bstNode
+{
+    int data;
+    struct bstNode *left;
+    struct bstNode *right;
+};
+struct bstNode *getNewNode(int x)
+{
+    struct bstNode *temp = (struct bstNode *)malloc(sizeof(struct bstNode));
+    temp->data = x;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+struct bstNode *insertNode(struct bstNode *node, int x)
+{
+    if (!node)
+    {
+        node = getNewNode(x);
+        return node;
+    }
+    if (x <= node->data)
+        node->left = insertNode(node->left, x);
+    if (x > node->data)
+        node->right = insertNode(node->right, x);
+    return node; //vvi
+}
+
+void printSpace(int x)
+{
+    for (int i = 0; i < x; i++)
+    {
+        printf(" ");
+    }
+    return;
+}
+
+void printBst(struct bstNode *node, int spaceCount)
+{
+    // printf("\nhello\n");
+    // printf("%d", node->data);
+    // printf("\n");
+
+    if (!node)
+        printf("Error: can't print bst is empty.\n");
+    if (node->right)
+    {
+        printBst(node->right, spaceCount + 10);
+    }
+    printSpace(spaceCount);
+    printf("%d\n", node->data);
+    if (node->left)
+    {
+        // printf("\nleft\n");
+        printBst(node->left, spaceCount + 10);
+    }
+    return;
+}
+
+bool searchBst(struct bstNode *node, int element, int level)
+{
+
+    if (!node)
+    {
+        return 0;
+    }
+    if (element == node->data)
+    {
+        printf("Found at level %d\n ", level);
+        return 1;
+    }
+    if (element <= node->data)
+        return searchBst(node->left, element, level + 1);
+
+    return searchBst(node->right, element, level + 1);
+}
+
+struct bstNode *findMin(struct bstNode *node)
+{
+    if (!node->left)
+    {
+        // printf("The smallest Element is \t%d\n",node->data);
+        return node;
+    }
+    return findMin(node->left);
+}
+struct bstNode *findMax(struct bstNode *node)
+{
+    if (!node->right)
+    {
+        // printf("The smallest Element is \t%d\n",node->data);
+        return node;
+    }
+    return findMax(node->right);
+}
+int max(int a, int b)
+{
+    return a >= b ? a : b;
+}
+int findHeight(struct bstNode *node)
+{
+    if (!node)
+        return -1;
+    return max(findHeight(node->left),findHeight(node->right)) + 1;
+}
+
+int main()
+{
+    struct bstNode *root = NULL;
+    int element;
+    while (1)
+    {
+        printf("What do you want to do to the BST\n1 => insertNode\n2 => search Element\n3 => printBst\n4 => findMin\n5 => findMax\n6 => findHeight of Bst\n");
+        scanf("%d", &element);
+
+        switch (element)
+        {
+        case 1:
+            printf("give us the element to insert\t");
+            scanf("%d", &element);
+            root = insertNode(root, element);
+            break;
+        case 2:
+            printf("give us the element to search\t");
+            scanf("%d", &element);
+            if (!searchBst(root, element, 0))
+                printf("NO match found \n");
+            break;
+        case 3:
+            printBst(root, 0);
+            break;
+        case 4:
+            findMin(root);
+            printf("The Samllest Element is %d with node adress of %d\n", findMin(root)->data, findMin(root));
+            break;
+        case 5:
+            findMax(root);
+            printf("The Largest Element is %d with node adress of %d\n", findMax(root)->data, findMax(root));
+            break;
+        case 6:
+            printf("The height of the the BST is %d\n", findHeight(root));
+            break;
+        default:
+            printf("Error: wrong Input\n");
+            break;
+        }
+
+        // printf("give us the element to insert\t");
+        // scanf("%d", &element);
+        // root = insertNode(root, element);
+        // printBst(root, 0);
+    }
+    // root = insertNode(root,15);
+    // insertNode(root,10);insertNode(root,20);
+    // insertNode(root,18);
+    // insertNode(root,9);
+    // insertNode(root,11);
+    // insertNode(root,38);
+
+    // printf("%d => %d|%d|%d\n",root,root->left,root->data,root->right);
+    // printf("%d => %d|%d|%d\n",root->left,root->left->left,root->left->data,root->left->right);
+    // printf("%d => %d|%d|%d\n",root->right,root->right->left,root->right->data,root->right->right);
+}
